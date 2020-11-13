9865b8b271696758b272d12109881962197bf7f3
Eder Sousa <ederwillian1880@gmail.com>
2020-11-12 21:56:41 -0300

    reuploading project

---
+++ Locadora.Filmes.Web/ViewModels/Filme/FilmeIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.Filmes.Web.ViewModels.Filme
+{
+    public class FilmeIndexViewModel
+    {
+        public long IdFilme { get; set; }
+        [Display(Name = "Nome do Filme")]
+        public string NomeFilme { get; set; }
+        [Display(Name = "Nome do álbum")]
+        public string NomeAlbum { get; set; }
+    }
+}
---
+++ Locadora.Filmes.Web/ViewModels/Filme/FilmeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Locadora.Filmes.Web.ViewModels.Filme
+{
+    public class FilmeViewModel
+    {
+        [Required(ErrorMessage = "Id Obrigatório")]
+        public long IdFilme { get; set; }
+
+        [Required(ErrorMessage = "Nome Obrigatório")]
+        [Display(Name = "Nome do Filme")]
+        public string NomeFilme { get; set; }
+
+        [Required(ErrorMessage = "Selecione um álbum")]
+        [Display(Name = "Nome do álbum")]
+        public int IdAlbum { get; set; }
+    }
+}
